4bf52fac807f6528cfd019d1f71e30fbcd544a03
Victoriia <kod.victoriya@gmail.com>
2025-09-09 09:47:38 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,17 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const totalMinDays = 3;
+  const totalMaxDays = 7;
+  const cost = 40;
+
+  if (days < totalMinDays) {
+    return days * cost;
+  } else if (days < totalMaxDays) {
+    return days * cost - 20;
+  } else {
+    return days * cost - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
